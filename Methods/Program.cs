aa51667da6ea940bc3f476e63ef88ee509ad042d
Nomfundo-Phororo <precious.phororo@gmail.com>
2020-02-18 14:25:58 +0200

    first commit

---
+++ Methods/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////scores array
+            //Console.WriteLine("How many scores do you want to insert: ");
+            //string size = Console.ReadLine();
+
+            //int answer;
+
+            //bool success = Int32.TryParse(size, out answer);
+
+
+
+
+            //if (success)
+            //{
+            //    double[] scores = new double[answer];
+
+            //    for (int i = 0; i < scores.Length; i++)
+            //    {
+            //        Console.WriteLine("Please enter score {0}: ", i + 1);
+            //        double score = double.Parse(Console.ReadLine());
+            //        if (score < 101)
+            //        {
+
+
+            //            scores[i] = Math.Round(score, 2);
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("Error!!! score is greater than 100");
+            //            break;
+            //        }
+
+
+
+            //    }
+
+
+            //    for (int i = 0; i < scores.Length; i++)
+            //    {
+            //        Console.Write(scores[i] + " | ");
+
+            //    }
+            //    Console.WriteLine(" ");
+            //    Console.WriteLine("___________STATISTICS__________");
+            //    Console.WriteLine($"The average is: {scores.Average()}");
+            //    Console.WriteLine($"The highest score is: {scores.Max()}");
+            //    Console.WriteLine($"The lowest score is: {scores.Min()}");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("ERROR MUST INSERT A NUMBER ");
+            //}
+
+
+            //triangle of 5s
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    for (int k = 1; k <= i; k++)
+            //    {
+            //        Console.Write(" " + k);
+
+            //    }
+            //    Console.WriteLine(" ");
+            //}
+            ////var fibonacciNumbers = new List<int> { 0, 1 };
+            //for (int i = 1; i <= 19; i++)
+            //{
+            //    var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+            //    var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
+            //    //fibonacciNumbers.Add(previous + previous2);
+
+
+            //}
+            //foreach (var item in fibonacciNumbers)
+            //    Console.WriteLine(item);
+            //Console.WriteLine(fibonacciNumbers.Count);
+
+
+            //int a, b, c;
+            //a = 0;
+            //b = 1;
+            //int[] fib = new int[20];
+            //for (int i = 1; i <fib.Length; i++)
+            //{
+            //    c = a + b;
+            //    a = b;
+            //    b = c;
+            //    fib[i] = c;
+
+            //}
+            //for (int j = 0; j < fib.Length; j++)
+            //{
+            //    Console.WriteLine(fib[j]);
+            //}
+
+            Console.Write("Enter any key to exit............");
+
+
+            Console.ReadKey();
+        }
+  
+    }
+}
